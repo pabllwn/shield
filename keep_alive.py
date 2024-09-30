9592b50a6ada7c2fdd11fa14eb1e1daa51e63929
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-30 22:07:14 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Bot is up and running!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    # إنشاء واستدعاء خيط للحفاظ على التطبيق قيد التشغيل في الخلفية
+    t = Thread(target=run)
+    t.start()
